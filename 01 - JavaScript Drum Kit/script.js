cb4a5ddbb854126cc73c6c62b05bbbc90c82575f
luanorlandi <orlandi.luan@gmail.com>
2018-04-07 14:49:01 -0300

    Day 3 CSS Variables

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,23 +1,39 @@
-const keyPress = (keyCode) => {
-  const audio = document.querySelector(`audio[data-key='${keyCode}'`);
-  const key = document.querySelector(`div[data-key='${keyCode}'`);
-
+const playAudio = audio => {
   if (!audio) {
     return;
   }
 
   audio.currentTime = 0;
   audio.play();
+}
+
+const keyPress = key => {
+  if (!key) {
+    return;
+  }
 
   key.classList.add('playing');
 }
 
-window.addEventListener('keydown', () => {
-  keyPress(event.keyCode);
+window.addEventListener('keydown', event => {
+  const key = document.querySelector(`div[data-key='${event.keyCode}'`);
+  const audio = document.querySelector(`audio[data-key='${event.keyCode}'`);
+
+  keyPress(key);
+  playAudio(audio);
 });
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', (event) => {
+
+keys.forEach(key => key.addEventListener('click', event => {
+  const keycode = key.getAttribute('data-key');
+  const audio = document.querySelector(`audio[data-key='${keycode}'`);
+
+  keyPress(key);
+  playAudio(audio);
+}));
+
+keys.forEach(key => key.addEventListener('transitionend', event => {
   if (event.propertyName !== 'transform') {
     return;
   }
